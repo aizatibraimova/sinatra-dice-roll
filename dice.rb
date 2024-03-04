2a057831777f3e5572e44a51dc4cacc7ee9a18c3
Aizat Ibraimova <97326728+aizatibraimova@users.noreply.github.com>
2024-03-04 16:17:46 +0000

    added better error gem

--- dice.rb
+++ dice.rb
@@ -1,5 +1,13 @@
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
+
 
 get("/") do
    "<h1>Dice Roll</h1>
